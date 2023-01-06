f9b0844aaf33c781074d03c5fe2cb7e001579360
Phillip Reynolds <preynolds@atp.com>
2023-01-06 15:38:30 -0600

    Completed Project 13 and Day 54.

---
+++ Project13/Project13/SliderType.swift
@@ -0,0 +1,14 @@
+//
+//  SliderType.swift
+//  Project13
+//
+//  Created by Phillip Reynolds on 1/6/23.
+//
+
+import Foundation
+
+enum SliderType {
+    case intensity
+    case radius
+    case scale
+}
